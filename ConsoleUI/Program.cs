da8e9097acde8946a43be65f2a1f7ccd44463a9c
alicankasap <alicankasap96@gmail.com>
2022-06-22 23:10:22 +0300

    9.Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,11 +10,24 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            //foreach (var product in productManager.GetAllByCategoryId(2))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
+
+            //Console.WriteLine(categoryManager.GetById(1).CategoryName);
         }
     }
 }
