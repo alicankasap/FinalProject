9a0fcef2d7947b7bf53a3472198618de711409e5
alicankasap <alicankasap96@gmail.com>
2022-07-26 21:53:43 +0300

    10.Gün Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -12,6 +13,8 @@ namespace ConsoleUI
             ProductManager productManager = new ProductManager(new EfProductDal());
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
+            //productManager.Add(new Product { CategoryId = 1, ProductName = "T", UnitPrice = 1000, UnitsInStock = 1 });
+
             //foreach (var product in productManager.GetAllByCategoryId(2))
             //{
             //    Console.WriteLine(product.ProductName);
@@ -22,12 +25,23 @@ namespace ConsoleUI
             //    Console.WriteLine(category.CategoryName);
             //}
 
-            foreach (var product in productManager.GetProductDetails())
-            {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
-            }
+            //foreach (var product in productManager.GetProductDetails())
+            //{
+            //    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            //}
 
             //Console.WriteLine(categoryManager.GetById(1).CategoryName);
+
+            var result = productManager.GetAll();
+
+            if (result.Success)
+            {
+                foreach (var product in productManager.GetAll().Data)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+            Console.WriteLine(result.Message);
         }
     }
 }
